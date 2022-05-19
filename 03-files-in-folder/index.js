d13444552d12a14e1f02e19bcb9cbff0046d093b
pdlmn <djeinwings@gmail.com>
2022-05-19 18:13:18 +0200

    feat: add 03-files-in-folder solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const fs = require('fs');
+
+(async () => {
+  const files = await fs.promises.readdir(
+    path.join(__dirname, 'secret-folder'),
+    { withFileTypes: true }
+  );
+  files.forEach(file => {
+    fs.stat(path.join(__dirname, 'secret-folder', file.name), (err, stats) => {
+      if (err) throw err
+      if (file.isDirectory()) return
+      const extension = path.extname(file.name)
+      console.log(`${path.basename(file.name, extension)} - ${extension.slice(1)} - ${(stats.size / 1000).toLocaleString()}kb`)
+    })
+  })
+})();
