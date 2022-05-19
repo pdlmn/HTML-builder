44930f588f323e381809952ea50073125cef80e0
pdlmn <djeinwings@gmail.com>
2022-05-19 16:24:53 +0200

    feat: add 01-read-file solution

---
+++ 01-read-file/index.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const fs = require('fs');
+
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+let data = '';
+stream.on('data', chunk => { 
+  data += chunk; 
+});
+stream.on('end', () => {
+  console.log(data.trim())
+})
