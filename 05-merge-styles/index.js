28d6c6daa0f8af2e6f8e015fd2f666619dcc7160
pdlmn <djeinwings@gmail.com>
2022-05-19 20:22:59 +0200

    feat: add 05-merge-styles solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const fs = require('fs/promises');
+
+const stylesDir = path.join(__dirname, 'styles');
+
+const makeBundle = async () => {
+  const files = await fs.readdir(stylesDir);
+  let content = '';
+  for (let file of files) {
+    if (path.extname(file) !== '.css') {
+      continue
+    }
+    content += await fs.readFile(path.join(stylesDir, file))
+    content += '\n'
+  }
+  content = content.trim()
+  fs.writeFile(path.join(__dirname, 'project-dist','bundle.css'), content)
+};
+
+makeBundle();
