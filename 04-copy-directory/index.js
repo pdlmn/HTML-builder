bbdab0f76ff07af9f67ffbdb444c9e4e6b26e5ca
pdlmn <djeinwings@gmail.com>
2022-05-19 19:22:30 +0200

    feat: add 04-copy-directory solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const path = require('path');
+const fs = require('fs');
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
 
+const copyDir = (files) => {
+  if (!files.includes('files-copy')) {
+    fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
+      if (err) throw err;
+    });
+  }
+  fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+    files.forEach(file => {
+      fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => {
+        if (err) throw err
+      })
+    })
+  })
+};
+
+const init = () => {
+  fs.readdir(__dirname, (err, files) => {
+    emitter.emit('filesLoaded', files);
+  })
+
+  emitter.on('filesLoaded', copyDir);
+}
+
+init();
