ab187131cc6ff5dfbaa11ce7d0c0dbb31d3a07b7
pdlmn <djeinwings@gmail.com>
2022-05-19 17:25:27 +0200

    feat: add 02-write-file solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const fs = require('fs');
+const EventEmitter = new require('events');
+const { stdout, stdin, exit } = process;
+const emitter = new EventEmitter();
+
+let filesArr;
+fs.readdir(path.join(__dirname), (err, files) => {
+  if (err) throw err;
+  filesArr = files;
+  emitter.emit('filesLoaded');
+});
+
+emitter.on('filesLoaded', () => {
+  if (!filesArr.includes('text.txt')) {
+    fs.appendFile(path.join(__dirname, 'text.txt'), '', (err) => {
+      if (err) throw err;
+      console.log('File was created successfully!');
+    });
+  }
+  console.log('Input data you want add to the file:');
+  stdin.on('data', (data) => {
+    if (data.toString().trim() === 'exit') {
+      console.log('Exitting the programm...')
+      exit();
+    }
+    fs.appendFile(path.join(__dirname, 'text.txt'), data, (err) => {
+      if (err) throw err;
+    });
+  });
+});
+
+process.on('SIGINT', () => { 
+  console.log('\nExitting the programm...');
+  exit()
+});
