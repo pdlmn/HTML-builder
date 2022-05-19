fd0bb081b57511dca3cd0b17dfeeef41dbd15d4a
pdlmn <djeinwings@gmail.com>
2022-05-19 18:57:42 +0200

    fix: apply linter

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -8,10 +8,12 @@ const fs = require('fs');
   );
   files.forEach(file => {
     fs.stat(path.join(__dirname, 'secret-folder', file.name), (err, stats) => {
-      if (err) throw err
-      if (file.isDirectory()) return
-      const extension = path.extname(file.name)
-      console.log(`${path.basename(file.name, extension)} - ${extension.slice(1)} - ${(stats.size / 1000).toLocaleString()}kb`)
-    })
-  })
+      if (err) throw err;
+      if (file.isDirectory()) return;
+      const extension = path.extname(file.name);
+      console.log(
+        `${path.basename(file.name, extension)} - ${extension.slice(1)} - ${(stats.size / 1000).toLocaleString()}kb`
+      );
+    });
+  });
 })();
