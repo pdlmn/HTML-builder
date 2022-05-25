31f9068da8db012d27c85253046db7ed7ceb72bf
pdlmn <djeinwings@gmail.com>
2022-05-25 13:47:14 +0200

    feat: make 04-copy-directory clean files before copying

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,12 +3,25 @@ const fs = require('fs');
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
-const copyDir = (files) => {
+const cleanDir = (files) => {
   if (!files.includes('files-copy')) {
     fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
       if (err) throw err;
     });
+  } else {
+    fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
+      files.forEach(file => {
+        fs.unlink(path.join(__dirname, 'files-copy', file), (err) => {
+          if (err) {
+            console.log(err);
+          }
+        });
+      });
+    });
   }
+};
+
+const copyDir = (files) => {
   fs.readdir(path.join(__dirname, 'files'), (err, files) => {
     files.forEach(file => {
       fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => {
@@ -22,6 +35,11 @@ const copyDir = (files) => {
 const init = () => {
   fs.readdir(__dirname, (err, files) => {
     emitter.emit('filesLoaded', files);
+  });
+
+  emitter.on('filesLoaded', (files) => {
+    cleanDir(files);
+    emitter.emit('directoryCreated', files);
   });
 
   emitter.on('filesLoaded', copyDir);
